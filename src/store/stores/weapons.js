fd261e6c8f6e5e9ffa8c63af295b89ea2650576a
Emil Carlsson <hello@emilcarlsson.se>
2023-11-08 23:59:57 +0100

    WIP: Prepare weapons view

---
+++ src/store/stores/weapons.js
@@ -0,0 +1,61 @@
+import { defineStore } from 'pinia'
+import defaultWeapons from '@/data/weapons'
+import camouflageNameChanges from '@/data/camouflageNameChanges'
+
+const defaultState = {
+	favorites: [],
+	weapons: [],
+}
+
+export const useWeaponsStore = defineStore('weapons', {
+	state: () => ({
+		...defaultState,
+	}),
+
+	getters: {
+		categories: (state) => Array.from(new Set(state.weapons.map((weapon) => weapon.category))),
+	},
+
+	actions: {
+		setFavorites(favorites) {
+			this.favorites = favorites || []
+		},
+
+		setWeapons(weapons) {
+			this.weapons = JSON.parse(JSON.stringify(defaultWeapons))
+
+			if (weapons) {
+				weapons.forEach((weapon) => {
+					const index = this.weapons.findIndex((w) => w.name === weapon.name)
+
+					if (index !== -1) {
+						// Set progress
+						Object.keys(weapon.progress).forEach((camouflage) => {
+							// Handle changes to camouflage names
+							if (camouflage in camouflageNameChanges && weapon.progress[camouflage]) {
+								this.weapons[index].progress[camouflageNameChanges[camouflage]] = true
+							} else if (camouflage in this.weapons[index].progress) {
+								this.weapons[index].progress[camouflage] = weapon.progress[camouflage]
+							}
+						})
+					}
+				})
+			}
+		},
+
+		toggleWeaponCompleted(weapon, current) {
+			const selectedWeapon = this.weapons.find((w) => w.name === weapon.name)
+			Object.keys(selectedWeapon.progress).forEach((camouflage) => {
+				selectedWeapon.progress[camouflage] = !current
+			})
+		},
+
+		resetFavorites() {
+			this.favorites = []
+		},
+
+		resetWeapons() {
+			this.weapons = JSON.parse(JSON.stringify(defaultWeapons))
+		},
+	},
+})
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
 			<br />
 
 			Want to contribute or join the discussion? Head over to our
-			<a href="https://github.com/carlssonemil/mw3/discussions/landing" target="_blank"
+			<a href="https://github.com/carlssonemil/interstellar/discussions/landing" target="_blank"
 				>GitHub discussions</a
 			>
 			and lend a hand or share your insights!<br />
@@ -17,5 +17,44 @@
 
 			Thank you for your understanding and support. Stay tuned for more updates! ❤️
 		</Alert>
+
+		<Weapons :weapons="filteredWeapons" :favorites="favorites" />
 	</div>
 </template>
+
+<script>
+import { mapState } from 'pinia'
+import { useWeaponsStore } from '@/store/stores/weapons'
+import { groupBy } from '@/utils/array'
+
+import Weapons from '@/components/Weapons.vue'
+
+export default {
+	components: {
+		Weapons,
+	},
+
+	computed: {
+		...mapState(useWeaponsStore, ['weapons', 'categories', 'favorites']),
+
+		filteredWeapons() {
+			return groupBy(this.weapons, 'category')
+		},
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+	text-align: center;
+}
+
+h1 {
+	margin-top: 75px;
+}
+
+h2 {
+	margin: 30px auto 0;
+	max-width: 450px;
+}
+</style>

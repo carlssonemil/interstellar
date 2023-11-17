9ec374b664eefc1c554fc6bc13912bb0586d95a9
Emil Carlsson <hello@emilcarlsson.se>
2023-11-17 18:55:47 +0100

    Add link to Zombies page in mobile menu

--- src/components/layout/MobileNavigationComponent.vue
+++ src/components/layout/MobileNavigationComponent.vue
@@ -11,9 +11,10 @@
       <div>
         <router-link to="/">{{ $tc('general.weapon', 2) }}</router-link>
         <router-link to="/camouflages">{{ $tc('general.camouflage', 2) }}</router-link>
+        <router-link to="/zombies">{{ $t('general.zombies') }}</router-link>
         <router-link to="/calling-cards">{{ $tc('general.calling_cards', 2) }}</router-link>
         <router-link to="/mastery">{{ $t('general.mastery') }}</router-link>
-        <router-link to="/requirements">{{ $tc('general.requirement', 2) }}</router-link>
+        <a href="https://orion.emca.app/">Orion</a>
       </div>
       <div class="footer">
         <router-link to="/settings">{{ $tc('general.setting', 2) }}</router-link>
@@ -23,6 +24,7 @@
           {{ $t('general.report_an_issue') }}
         </a>
         <router-link to="/about">{{ $t('general.about') }}</router-link>
+        <router-link to="/requirements">{{ $tc('general.requirement', 2) }}</router-link>
         <a href="https://github.com/carlssonemil/interstellar">GitHub</a>
 
         <a href="https://www.buymeacoffee.com/emilcarlsson" class="button">

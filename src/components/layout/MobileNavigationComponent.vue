2b8be49cdee375ab30e862b7501702103c106bc8
Emil Carlsson <hello@emilcarlsson.se>
2023-11-18 12:32:44 +0100

    Disable calling cards link in mobile nav

--- src/components/layout/MobileNavigationComponent.vue
+++ src/components/layout/MobileNavigationComponent.vue
@@ -12,7 +12,9 @@
         <router-link to="/">{{ $tc('general.weapon', 2) }}</router-link>
         <router-link to="/camouflages">{{ $tc('general.camouflage', 2) }}</router-link>
         <router-link to="/zombies">{{ $t('general.zombies') }}</router-link>
-        <router-link to="/calling-cards">{{ $tc('general.calling_cards', 2) }}</router-link>
+        <router-link to="/calling-cards" class="coming-soon">
+          <span>{{ $tc('general.calling_cards', 2) }}</span>
+        </router-link>
         <router-link to="/mastery">{{ $t('general.mastery') }}</router-link>
         <a href="https://orion.emca.app/">Orion</a>
       </div>
